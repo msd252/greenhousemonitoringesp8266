56ec93112d3db4537ad4c38d8762386ef4f94ca9
msd252 <charlie@palin.pw>
2018-10-20 21:16:44 +0100

    initial commit

---
+++ greenhousemonitor_esp8266.ino
@@ -0,0 +1,74 @@
+/*
+    HTTP over TLS (HTTPS) example sketch
+    This example demonstrates how to use
+    WiFiClientSecure class to access HTTPS API.
+    We fetch and display the status of
+    esp8266/Arduino project continuous integration
+    build.
+    Limitations:
+      only RSA certificates
+      no support of Perfect Forward Secrecy (PFS)
+      TLSv1.2 is supported since version 2.4.0-rc1
+    Created by Ivan Grokhotkov, 2015.
+    This example is in public domain.
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WEMOS_SHT3X.h>
+
+SHT3X sht30(0x45);
+
+const char* ssid = "........";
+const char* password = "........";
+
+const char* host = "api.github.com";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "5F F1 60 31 09 04 3E F2 90 D2 B0 8A 50 38 04 E8 37 9F BC 76";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+   // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+    Serial.println("certificate matches");
+  } else {
+    Serial.println("certificate doesn't match");
+  }
+
+  
+  const String post =   "POST /temp HTTP/1.1\r\n"
+                          "User-Agent: Arduino/1.0\r\n"
+                          "Connection: close\r\n"
+                          "Content-Type: application/x-www-form-urlencoded;\r\n"
+                          "\r\n"
+                          "humidity=" + String(sht30.cTemp) + "&temp=" + String(sht30.humidity) + "\n";
+
+  client.print(post);
+}
